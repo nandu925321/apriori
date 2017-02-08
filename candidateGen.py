a58302aa8c9e610f215d1a54c2eb369cd2a803ce
krishnaBharadwaj <krishnaknbharadwaj@gmail.com>
2017-02-07 22:04:17 -0600

    MS apriori candidate gen and level2 candidate gen functions

--- candidateGen.py
+++ candidateGen.py
@@ -1,12 +1,12 @@
 import numpy as np
-def candidateGen (Fkprev):
+def candidateGen (Fkprev, MIS, sup, MSD):
     npFkprev = np.asarray(Fkprev)
     C = []
     for i in range(len(npFkprev) - 1):
         for j in range(i+1, len(npFkprev)):
             f1 = npFkprev[i]
             f2 = npFkprev[j]
-            if np.array_equal(f1[:-1],f2[:-1]):
+            if np.array_equal(f1[:-1],f2[:-1]) and abs(sup[f1[-1]] - sup[f2[-1]]) <= MSD:
                 if (f1[-1] < f2[-1]):
                     c = np.append(f1,[f2[-1]])
                 else:
@@ -16,15 +16,25 @@ def candidateGen (Fkprev):
                 include = True
                 for k in range(len(c)):
                     p.mask[k] = True
-                    pc = p.compressed()
+                    s = p.compressed()
                     p.mask[k] = False
-                    #print pc
-                    if (npFkprev[...]==pc).all(1).any() == False:
-                        include = False
+                    if ((c[0] == s).any() == True) or (MIS[c[1]] == MIS[c[0]]):
+                        if (npFkprev[...]==s).all(1).any() == False:
+                            include = False
                 if include == True:
                     C.append(c.tolist())
     return C
 
 
-
+#where L is the output from InitPass function, countList is the list of counts of all items, MIS is the 
+def level2CandidateGen (L, MIS, sup, MSD):
+    C2 = []
+    for i in range(len(L)):
+        l = L[i]
+        if sup[l] >= MIS[l]:
+            for j in range(i+1, len(L)):
+                h = L[j]
+                if sup[h] >= MIS[l] and abs(sup[h] - sup[l]) <= MSD:
+                    C2.append([l,h])
+    return C2
 
--- test.py
+++ test.py
@@ -2,8 +2,20 @@ from candidateGen import *
 
 #Values taken from text book, page number 22, example 3.
 a = [[1, 2, 3], [1, 2, 4], [1, 3, 4], [1, 3, 5], [2, 3, 4]]
-b = [['apple', 'ball', 'cat'], ['apple', 'ball', 'dog'], ['apple', 'cat', 'dog'], ['apple', 'cat', 'egg'], ['ball', 'cat', 'dog']]
-res = candidateGen(a)
+#b = [['apple', 'ball', 'cat'], ['apple', 'ball', 'dog'], ['apple', 'cat', 'dog'], ['apple', 'cat', 'egg'], ['ball', 'cat', 'dog']]
+MIS = {1:0.1, 2:0.2, 3:0.05, 4:0.06, 5:0.001}
+sup = {1:0.8, 2:0.6, 3:0.8, 4:0.6, 5:0.2}
+MSD = 0.4
+res = candidateGen(a, MIS, sup, MSD)
 print res
-res = candidateGen(b)
+#res = candidateGen(b, MIS, sup, MSD)
+#print res
+L = [ 100, 140, 90, 60, 50 ]
+counts = {100: 1, 140: 1, 80:5, 90:2, 60:0, 50:2}
+MIS = {100: 0.10, 140: 0.15, 80: 0.20, 90: 0.20, 60: 0.30, 50: 0.40}
+sup = {100: 1/6.0, 140: 1/6.0, 80: 5/6.0, 90: 2/6.0, 60: 0, 50: 2/6.0}
+print sup
+res2 = level2CandidateGen (L, MIS, sup, MSD)
+res = candidateGen(res2, MIS, sup, MSD)
+print res2
 print res
